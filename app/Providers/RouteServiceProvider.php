c685b5a4d1d8934351de2ee831680350bbc9eef5
toilacube <151115rubikcube@gmail.com>
2023-12-22 12:53:54 +0700

    remove api middleware

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,8 +29,7 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
+            Route::prefix('api')
                 ->group(base_path('routes/api.php'))
                 ->group(base_path('routes/api/Products/Products.api.php'))
                 ->group(base_path('routes/api/Products/ProductItem.api.php'))
